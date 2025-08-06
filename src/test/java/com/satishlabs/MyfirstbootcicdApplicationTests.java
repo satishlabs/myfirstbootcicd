a5a9be3e7a881db8b936832239920195854a195e
satishlabs <spd61185@gmail.com>
2025-08-06 07:34:49 +0530

    Initial commit

---
+++ src/test/java/com/satishlabs/MyfirstbootcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satishlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstbootcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
